dfedd838ae0cd3e6b0f95aa35d041fdc9d7a7ded
Aissaoui El Mokhtar <el-mokhtar.aissaoui@etu.umontpellier.fr>
2021-02-14 11:02:54 +0100

    Update stageRoutes.js

--- stageinfo-backend/routes/stageRoutes.js
+++ stageinfo-backend/routes/stageRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const stageCtrl = require('../controllers/stageController');
 
 router.get('/', stageCtrl.getAllStage);
-
+router.get('/:id',stageCtrl.getOneStage);
+router.post('/',stageCtrl.createStage);
+router.put('/:id',stageCtrl.modifyThing);
 
 module.exports = router;
