2154543b18cce08e23be157ce40d50fadecbbdfd
Antoine Brahimi <antoine.brahimi@etu.umontpellier.fr>
2021-02-14 20:49:07 +0100

    Ajout des requetes get:id, put, delte:id, delete pour la collection Salle

--- stageinfo-backend/routes/salleRoutes.js
+++ stageinfo-backend/routes/salleRoutes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 const salleCtrl = require('../controllers/salleController');
 
+router.get('/:id', salleCtrl.getOneSalle);
 router.get('/', salleCtrl.getAllSalle);
-router.post('/', salleCtrl.postSalle);
+router.post('/', salleCtrl.createSalle);
+router.put('/:id', salleCtrl.editSalle);
+router.delete('/:id', salleCtrl.deleteOneSalle);
+router.delete('/', salleCtrl.deleteAllSalle);
 
 module.exports = router;
