b369415fa788639336694807245d9d24927413ce
Aissaoui El Mokhtar <el-mokhtar.aissaoui@etu.umontpellier.fr>
2021-02-16 15:44:56 +0100

    Update userModel.js

--- stageinfo-backend/models/userModel.js
+++ stageinfo-backend/models/userModel.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const uniqueValidator = require('mongoose-unique-validator');
 const userSchema = Schema({
     nom : String,
     prenom : String,
-    email : String,
+    email : {type: String, required: true},
     telephone : Number,
     fax : Number,
     hash : String, //password
-    role : String,
+    role : {type: String, required: true},
     
     //etudiant
     numeroEtudiant : String,
@@ -19,5 +19,6 @@ const userSchema = Schema({
     fonctionOccupee : String,
     idEntreprise : Schema.Types.ObjectId,
   });
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
