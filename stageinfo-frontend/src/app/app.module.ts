3d3068219d566b2c96e74fc2d1b7d9bf449bcffa
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-02-16 14:50:24 +0100

    Début de la construction de l'interface : ajout sidebar + Ajout font poppins + déplacement .ico dans le fichier assets

--- stageinfo-frontend/src/app/app.module.ts
+++ stageinfo-frontend/src/app/app.module.ts
@@ -4,21 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-//import { CoreModule } from './core/core.module';
 
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+//import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
     //CoreModule
   ],
   providers: [],
