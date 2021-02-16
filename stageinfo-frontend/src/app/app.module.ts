762c95d0c936d285cf6304bebd1d28b83538a0cb
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-02-16 11:56:49 +0100

    Modification de la structure du projet angular + ajout bootstrap

--- stageinfo-frontend/src/app/app.module.ts
+++ stageinfo-frontend/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//import { CoreModule } from './core/core.module';
+
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
