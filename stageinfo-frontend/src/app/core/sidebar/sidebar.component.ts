d792d45502e8b97cdb45810a575da7f99d1416dc
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-01 14:59:32 +0100

    changement lien route dans la sidebar

--- stageinfo-frontend/src/app/core/sidebar/sidebar.component.ts
+++ stageinfo-frontend/src/app/core/sidebar/sidebar.component.ts
@@ -11,32 +11,32 @@ export class SidebarComponent implements OnInit {
     {
       title:"Stages",
       items: [
-        {icon:"fas fa-list", nom:"Listing des stages", lien:"/", items:[]},
-        {icon:"fas fa-user-friends", nom:"Étudiant sans stages", lien:"/", items:[]},
-        {icon:"fas fa-pencil-alt", nom:"Proposer un stage", lien:"/", items:[]},
-        {icon:"fas fa-table", nom:"Les notes des stages", lien:"/", items:[]},
+        {icon:"fas fa-list", nom:"Listing des stages", lien:"/listing-stages", items:[]},
+        {icon:"fas fa-user-friends", nom:"Étudiant sans stages", lien:"/etudiant-sans-stages", items:[]},
+        {icon:"fas fa-pencil-alt", nom:"Proposer un stage", lien:"/saisir-stage", items:[]},
+        {icon:"fas fa-table", nom:"Les notes des stages", lien:"/note-de-stage", items:[]},
         {icon:"fas fa-file-csv", nom:"Exporter CSV", lien:"/", items:[
-          {icon:"", nom:"Les stages", lien:"/", items:[]},
-          {icon:"", nom:"Les soutenances", lien:"/", items:[]},
-          {icon:"", nom:"Les notes de stage", lien:"/", items:[]},
+          {icon:"", nom:"Les stages", lien:"/stages-csv", items:[]},
+          {icon:"", nom:"Les soutenances", lien:"/soutenances-csv", items:[]},
+          {icon:"", nom:"Les notes de stage", lien:"/notes-de-stage-csv", items:[]},
         ]},
         {icon:"fas fa-download", nom:"Télécharger", items:[
-          {icon:"fas fa-border-all", nom:"Feuille de stage", lien:"/", items:[]},
-          {icon:"", nom:"Fiche entreprise", lien:"/", items:[]},
-          {icon:"", nom:"Fiche de notation", lien:"/", items:[]},
+          {icon:"fas fa-border-all", nom:"Feuille de stage", lien:"/fiche-stage", items:[]},
+          {icon:"", nom:"Fiche entreprise", lien:"/fiche-entreprise", items:[]},
+          {icon:"", nom:"Fiche de notation", lien:"/fiche-notation", items:[]},
         ]}
       ] 
     },
     {
       title:"Soutenances",
       items: [
-        {icon:"fas fa-list", nom:"Listing des soutenances", lien:"/", items:[]},
-        {icon:"fas fa-list", nom:"Listing des soutenances non planifiées", lien:"/", items:[]},
-        {icon:"fas fa-calendar-alt", nom:"Planing de soutenance", lien:"/", items:[]},
+        {icon:"fas fa-list", nom:"Listing des soutenances", lien:"/listing-soutenance", items:[]},
+        {icon:"fas fa-list", nom:"Listing des soutenances non planifiées", lien:"/listing-soutenance", items:[]},
+        {icon:"fas fa-calendar-alt", nom:"Planing de soutenance", lien:"/planing-soutenance", items:[]},
 
         //{icon:"fas fa-comment", nom:"Télécharger fiche entreprise", lien:"/", items:[]},
         //{icon:"fas fa-database", nom:"Saisir fiche suivie étudiant", lien:"/", items:[]},
-        {icon:"fas fa-pencil-alt", nom:"Saisir fiche de suivie", lien:"/", items:[]},
+        {icon:"fas fa-pencil-alt", nom:"Saisir fiche de suivie", lien:"/saisir-fiche-suivi", items:[]},
         //{icon:"fas fa-file", nom:"Télécharger la fiche de notation", lien:"/", items:[]},
         //{icon:"fas fa-file-alt", nom:"Export des soutenances", lien:"/", items:[]},
       ] 
@@ -44,32 +44,32 @@ export class SidebarComponent implements OnInit {
     {
       title:"Statistiques",
       items: [
-        {icon:"fas fa-chart-bar", nom:"Suivi d'embauche", lien:"/", items:[]},
-        {icon:"fas fa-clipboard-list", nom:"Classement entreprises", lien:"/", items:[]},
-        {icon:"fas fa-chart-pie", nom:"Tendances-Calcul d'occurences", lien:"/", items:[]},
-        {icon:"fas fa-server", nom:"Etiquetage", lien:"/", items:[]}
+        {icon:"fas fa-chart-bar", nom:"Suivi d'embauche", lien:"/suivi-embauche", items:[]},
+        {icon:"fas fa-clipboard-list", nom:"Classement entreprises", lien:"/classement-entreprise", items:[]},
+        {icon:"fas fa-chart-pie", nom:"Tendances-Calcul d'occurences", lien:"/tendance-truc", items:[]},
+        {icon:"fas fa-server", nom:"Etiquetage", lien:"/etiquetage", items:[]}
       ]
     },
     {
       title:"Archivages",
       items: [
-        {icon:"fas fa-file-archive", nom:"Rapports téléchargeables", lien:"/", items:[]},
-        {icon:"fas fa-book", nom:"Stages années précédentes", lien:"/", items:[]}
+        {icon:"fas fa-file-archive", nom:"Rapports téléchargeables", lien:"/rapports-de-stages", items:[]},
+        {icon:"fas fa-book", nom:"Stages années précédentes", lien:"/archive-stages", items:[]}
       ]
     },
     {
       title:"Vous êtes développeur ?",
       items: [
         {icon:"fas fa-book", nom:"Documentation", lien:"/documentation", items:[]},
-        {icon:"fab fa-gitlab", nom:"Lien du GitLab", lien:"/", items:[]},
-        {icon:"fas fa-comment", nom:"Contacter le support", lien:"/", items:[]},
+        {icon:"fab fa-gitlab", nom:"Lien du GitLab", lien:"/gitlab", items:[]},
+        {icon:"fas fa-comment", nom:"Contacter le support", lien:"/support", items:[]},
       ]
     },
     {
       title:"Besoin d'aide ?",
       items: [
-        {icon:"fas fa-question", nom:"FAQ", lien:"/", items:[]},
-        {icon:"fas fa-comment", nom:"Nous contacter", lien:"/", items:[]},
+        {icon:"fas fa-question", nom:"FAQ", lien:"/faq", items:[]},
+        {icon:"fas fa-comment", nom:"Nous contacter", lien:"/nous-contacter", items:[]},
         //{icon:"fas fa-phone", nom:"Nous contacter", lien:"/", items:[]},
       ] 
     }
