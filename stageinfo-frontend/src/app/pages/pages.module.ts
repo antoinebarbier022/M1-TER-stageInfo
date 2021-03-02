4116720959607012331ee8a7be51bddbbcbe82f3
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-02 17:23:59 +0100

    Correction importation module et component

--- stageinfo-frontend/src/app/pages/pages.module.ts
+++ stageinfo-frontend/src/app/pages/pages.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router';
 
 import { Error404Component } from './error404/error404.component';
 import { ExempleTemplateComponent } from './exempleTemplate/exempleTemplate.component';
 
-//import mes module
 import { UserModule } from './user/user.module';
-import { DocumentationModule } from './documentation/documentation.module';
+
+
 
 @NgModule({
-  declarations: [ExempleTemplateComponent, Error404Component,],
+  declarations: [
+    ExempleTemplateComponent, 
+    Error404Component, 
+   ],
   imports: [
     CommonModule,
-    UserModule,
-    DocumentationModule
-  ], exports:[ExempleTemplateComponent, Error404Component, UserModule, DocumentationModule],
+    BrowserModule,
+    RouterModule,
+    UserModule
+  ], exports:[
+    ExempleTemplateComponent, 
+    Error404Component, 
+    UserModule],
 })
 export class PagesModule { }
