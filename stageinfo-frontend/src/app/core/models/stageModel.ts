5e30442150e652c4339b64260a52dfca1a271712
Antoine Brahimi <antoine.brahimi@etu.umontpellier.fr>
2021-03-03 18:06:45 +0100

    Ajout du model Stage

--- stageinfo-frontend/src/app/core/models/stageModel.ts
+++ stageinfo-frontend/src/app/core/models/stageModel.ts
@@ -27,16 +27,48 @@ export class stageSchema = {
     noteStage: {
         date: Date;
         valeur: string;
+        commentaire: string;
+    };
 
+    parcours: {
+        idParcours: string;
+        nomComplet: string;
+    };
+
+    ajouteur: {
+        idAjouteur: string;
+        nomComplet: string;
+    };
+
+    entreprise: {
+        idEntreprise: string;
+        nomComplet: string;
+    };
+
+    tuteurUniv: {
+        idTuteurUniv: string;
+        nomComplet: string;
+    };
+
+    tuteurEntreprise: {
+        idTuteurEntreprise: string;
+        nomComplet: string;
     }
 
+    rapporteur: { 
+        idRapporteur: string;
+        nomComplet: string;
+    }
 
+    etudiant: {
+        idEtudiant: string;
+        nomComplet: string;
+    }
 
-
-
-
-
-
-
+    idVisite: {
+        typeContact: string;
+        dateVisite: Date;
+        commentaire: string;
+    }
     
 }
