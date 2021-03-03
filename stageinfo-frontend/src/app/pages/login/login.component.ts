78003301a39086bae5ac8094a63fbccd3a3dbf90
El-Mokhtar-AIssaoui <78304363+El-Mokhtar-AIssaoui@users.noreply.github.com>
2021-03-03 01:56:16 +0100

    partie type script du login

--- stageinfo-frontend/src/app/pages/login/login.component.ts
+++ stageinfo-frontend/src/app/pages/login/login.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  // @ts-ignore
+  loginForm: FormGroup;
+  loading = false;
+  // @ts-ignore
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private auth: AuthService,
+            ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required]
+    });
+  }
+
+  onLogin() {
+    this.loading = true;
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+    this.auth.login(email, password).then(
+      () => {
+        this.loading = false;
+        this.router.navigate(['/']);
+      }
+    ).catch(
+      (error: { message: string; }) => {
+        this.loading = false;
+        this.errorMessage = error.message;
+      }
+    );
   }
 
 }
