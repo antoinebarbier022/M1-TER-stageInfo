f5f8d2eb168432f69776f5201b8eba08b084783b
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-04 19:44:53 +0100

    interface simpliste importer utilisateurs

--- stageinfo-frontend/src/app/app-routing.module.ts
+++ stageinfo-frontend/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { InfoUserComponent } from './pages/user/info-user/info-user.component';
 import { EditUserComponent } from './pages/user/edit-user/edit-user.component';
 import { EditUserV2Component } from './pages/user/edit-user-v2/edit-user-v2.component';
 import { AddUserComponent } from './pages/user/add-user/add-user.component';
+import { ImportUsersComponent } from './pages/user/import-users/import-users.component';
 import { ListUsersComponent } from './pages/user/list-users/list-users.component';
 import { ListStagesComponent } from './pages/stage/list-stages/list-stages.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -24,6 +25,7 @@ const routes: Routes = [
   { path: 'profile', component: ProfileUserComponent, canActivate: []},
   { path: 'liste-utilisateurs/user', component: InfoUserComponent, canActivate: []},
   { path: 'liste-utilisateurs', component: ListUsersComponent, canActivate: []},
+  { path: 'liste-utilisateurs/import-users', component: ImportUsersComponent, canActivate: []},
   { path: 'liste-utilisateurs/add-user', component: AddUserComponent, canActivate: []},
   { path: 'liste-utilisateurs/edit-user', component: EditUserComponent, canActivate: []},
   { path: 'liste-utilisateurs/edit-user-v2', component: EditUserV2Component, canActivate: []},
--- stageinfo-frontend/src/app/pages/user/user.module.ts
+++ stageinfo-frontend/src/app/pages/user/user.module.ts
@@ -9,10 +9,11 @@ import { ListUsersComponent } from './list-users/list-users.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { EditUserV2Component } from './edit-user-v2/edit-user-v2.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { ImportUsersComponent } from './import-users/import-users.component';
 
 
 @NgModule({
-  declarations: [ProfileUserComponent, InfoUserComponent, ListUsersComponent, EditUserComponent, EditUserV2Component, AddUserComponent],
+  declarations: [ProfileUserComponent, InfoUserComponent, ListUsersComponent, EditUserComponent, EditUserV2Component, AddUserComponent, ImportUsersComponent],
   imports: [
     CommonModule,
     BrowserModule,
