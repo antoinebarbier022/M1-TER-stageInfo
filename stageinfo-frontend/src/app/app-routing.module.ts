28f7be8d40c4b510d5ff76b8bd8350cc167c29cb
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-04 18:43:28 +0100

    Ajout component add-user

--- stageinfo-frontend/src/app/app-routing.module.ts
+++ stageinfo-frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ProfileUserComponent } from './pages/user/profile-user/profile-user.com
 import { InfoUserComponent } from './pages/user/info-user/info-user.component';
 import { EditUserComponent } from './pages/user/edit-user/edit-user.component';
 import { EditUserV2Component } from './pages/user/edit-user-v2/edit-user-v2.component';
+import { AddUserComponent } from './pages/user/add-user/add-user.component';
 import { ListUsersComponent } from './pages/user/list-users/list-users.component';
 import { ListStagesComponent } from './pages/stage/list-stages/list-stages.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -23,6 +24,7 @@ const routes: Routes = [
   { path: 'profile', component: ProfileUserComponent, canActivate: []},
   { path: 'liste-utilisateurs/user', component: InfoUserComponent, canActivate: []},
   { path: 'liste-utilisateurs', component: ListUsersComponent, canActivate: []},
+  { path: 'liste-utilisateurs/add-user', component: AddUserComponent, canActivate: []},
   { path: 'liste-utilisateurs/edit-user', component: EditUserComponent, canActivate: []},
   { path: 'liste-utilisateurs/edit-user-v2', component: EditUserV2Component, canActivate: []},
   { path: 'not-found', component: Error404Component, canActivate: []},
--- stageinfo-frontend/src/app/pages/user/user.module.ts
+++ stageinfo-frontend/src/app/pages/user/user.module.ts
@@ -8,10 +8,11 @@ import { InfoUserComponent } from './info-user/info-user.component';
 import { ListUsersComponent } from './list-users/list-users.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { EditUserV2Component } from './edit-user-v2/edit-user-v2.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 
 @NgModule({
-  declarations: [ProfileUserComponent, InfoUserComponent, ListUsersComponent, EditUserComponent, EditUserV2Component],
+  declarations: [ProfileUserComponent, InfoUserComponent, ListUsersComponent, EditUserComponent, EditUserV2Component, AddUserComponent],
   imports: [
     CommonModule,
     BrowserModule,
