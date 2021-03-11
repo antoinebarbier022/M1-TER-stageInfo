632d849704363cc30f6d3926bce4e7fc738f77ee
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-11 16:21:36 +0100

    Ajout ngFor dans liste-entreprise

--- stageinfo-frontend/src/app/pages/entreprise/list-entreprises/list-entreprises.component.ts
+++ stageinfo-frontend/src/app/pages/entreprise/list-entreprises/list-entreprises.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListEntreprisesComponent implements OnInit {
   title = "Liste des entreprises"
-  
+
+  // l'id de l'entreprise sert à afficher les informations de l'entreprise lors du clique sur le lien
+  entreprises = [
+    { id:"1", nom:"IBM", ville:"Montpellier", representant:"André Fernand"},
+    { id:"2", nom:"Ubisoft", ville:"Montpellier", representant:"Aymeric Dumont"},
+    { id:"3", nom:"Cap Gemini", ville:"Paris", representant:"Pascal Legrand-frère"},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
