5036ff50dc22d69156183b6ade64d54e6a32b819
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-16 12:34:41 +0100

    Ajout css pour les commentaires sur les stages info

--- stageinfo-frontend/src/app/pages/stage/info-stage/info-stage.component.ts
+++ stageinfo-frontend/src/app/pages/stage/info-stage/info-stage.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info-stage.component.scss']
 })
 export class InfoStageComponent implements OnInit {
-  title="Stage : Nom du stage"
+  title="Stage : Nom du stage";
+
+  comments = [{ author:"Antoine", 
+    date:"05/04/2018", 
+    content:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem a, ex voluptates aut saepe culpa, porro deserunt assumenda id quibusdam temporibus molestias quia, accusantium adipisci quasi voluptatem. Architecto, quibusdam asperiores?"
+  },
+  { author:"Henry", 
+  date:"05/04/2018", 
+  content:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem a, ex voluptates aut saepe culpa, porro deserunt assumenda id quibusdam temporibus molestias quia, accusantium adipisci quasi voluptatem. Architecto, quibusdam asperiores?"
+}];
+
   constructor() { }
 
   ngOnInit(): void {
