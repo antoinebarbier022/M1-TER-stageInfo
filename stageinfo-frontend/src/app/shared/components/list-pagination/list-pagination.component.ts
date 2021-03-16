372ff57570d3fcbaeac9c2db34509e59425b5a14
Antoine Brahimi <antoine.brahimi@etu.umontpellier.fr>
2021-03-16 15:14:35 +0100

    modificaiton ngOnInit

---
+++ stageinfo-frontend/src/app/shared/components/list-pagination/list-pagination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pagination',
+  templateUrl: './list-pagination.component.html',
+  styleUrls: ['./list-pagination.component.scss']
+})
+export class ListPaginationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
