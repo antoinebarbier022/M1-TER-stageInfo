1331bf4799abf52f95467dc8903007ce55dfbb61
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-17 20:50:03 +0100

    Suppression du supperflux dans liste stage.ts

--- stageinfo-frontend/src/app/pages/stage/info-stage/info-stage.component.ts
+++ stageinfo-frontend/src/app/pages/stage/info-stage/info-stage.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-import { StageService } from 'src/app/core/services/stage.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-info-stage',
   templateUrl: './info-stage.component.html',
   styleUrls: ['./info-stage.component.scss']
 })
-export class InfoStageComponent implements OnInit, OnDestroy {
-  // récupération du contenu du stage
+export class InfoStageComponent implements OnInit {
   stage : any;
-  loading:any;
 
   comments = [
     { idUser:"1",
@@ -27,35 +22,11 @@ export class InfoStageComponent implements OnInit, OnDestroy {
     content:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem a, ex voluptates aut saepe culpa, porro deserunt assumenda id quibusdam temporibus molestias quia, accusantium adipisci quasi voluptatem. Architecto, quibusdam asperiores?"
 }];
 
-  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private route:ActivatedRoute,
-              private router: Router,
-              private stageService: StageService) { 
-  }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.stage = this.route.snapshot.data.stage;  
-    //this.getStage(this.route.snapshot.paramMap.get('id'));
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next(true);
-    this.destroy$.unsubscribe();
-  }
-
-  getStage(id:any) {
-    this.stageService.getStageById(id)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((_stage: any[]) => {
-        this.stage = _stage;
-      }, (_error:any) =>{
-        // redirection vers la page d'erreur 404 si le stage n'est pas trouvé
-        if(_error.status == "404"){
-          this.router.navigate(['not-found']);
-        }
-      }
-    );
   }
 
 }
