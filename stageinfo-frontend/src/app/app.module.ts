0f8c5b57984f609097975c3604edaea6f0b4920e
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-17 08:21:55 +0100

    Ajout d'un package bar de loading + ajout au projet

--- stageinfo-frontend/src/app/app.module.ts
+++ stageinfo-frontend/src/app/app.module.ts
@@ -6,6 +6,13 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// for Router import:
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+
+// for Core import:
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+
+
 // importation mes modules
 import { PagesModule } from './pages/pages.module';
 import { CoreModule } from './core/core.module';
@@ -23,6 +30,10 @@ import { AuthInterceptor } from './core/interceptors/auth-interceptor';
     HttpClientModule,
     AppRoutingModule,
     CommonModule,
+    // Loading bar
+    LoadingBarRouterModule,
+    LoadingBarModule,
+    
     PagesModule,
     CoreModule,
     SharedModule
