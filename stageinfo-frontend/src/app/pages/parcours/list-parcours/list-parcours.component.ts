601583fc9de1fa8015f9cf5411ee285be1c3a875
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-17 10:40:48 +0100

    Ajout liste des parcours (interface version simple)

--- stageinfo-frontend/src/app/pages/parcours/list-parcours/list-parcours.component.ts
+++ stageinfo-frontend/src/app/pages/parcours/list-parcours/list-parcours.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-parcours.component.scss']
 })
 export class ListParcoursComponent implements OnInit {
+  title="Liste des parcours"
 
+  allParcours = [
+    {
+      acronyme:"AIglE", 
+      intitule:"Architecture et ingénierie du logiciel et du web",
+      niveau:"Master 1", 
+      description:"...",
+      responsable:"Pierre pompidor"
+    },
+    {
+        acronyme:"MIT", 
+      intitule:"Maths Informatique",
+      niveau:"Master 1", 
+      description:"...",
+      responsable:"inconnu"
+    },
+    {
+      acronyme:"Imagina", 
+      intitule:"Images et Jeux vidéos",
+      niveau:"Master 1", 
+      description:"...",
+      responsable:"inconnu"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
