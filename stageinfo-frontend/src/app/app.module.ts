e814dc9f1a15deec2a4bbcc12e4f2549002b9f29
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-03-17 08:40:42 +0100

    paramétrage bar de chargement pour qu'elle s'affiche seulement quand le temps de chargement n'est pas instantanné

--- stageinfo-frontend/src/app/app.module.ts
+++ stageinfo-frontend/src/app/app.module.ts
@@ -8,9 +8,9 @@ import { AppComponent } from './app.component';
 
 // for Router import:
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
-
 // for Core import:
 import { LoadingBarModule } from '@ngx-loading-bar/core';
+import { LOADING_BAR_CONFIG } from '@ngx-loading-bar/core';
 
 
 // importation mes modules
@@ -33,12 +33,16 @@ import { AuthInterceptor } from './core/interceptors/auth-interceptor';
     // Loading bar
     LoadingBarRouterModule,
     LoadingBarModule,
-    
+
     PagesModule,
     CoreModule,
     SharedModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    // Permet de commencer la bar de chargement un peu plus tard (quand ça commence à faire long)
+    { provide: LOADING_BAR_CONFIG, useValue: { latencyThreshold: 100 } }
+  ],
   bootstrap: [AppComponent]
 })
 
