baa62535cce892700ab3fd3d7c2e7ddd31968f40
Antoine Brahimi <antoine.brahimi@etu.umontpellier.fr>
2021-03-18 20:27:48 +0100

    Création de la super classe de lsiting de table

---
+++ stageinfo-frontend/src/app/shared/classes/common-listing-table.spec.ts
@@ -0,0 +1,7 @@
+import { CommonListingTable } from './common-listing-table';
+
+describe('CommonListingTable', () => {
+  it('should create an instance', () => {
+    expect(new CommonListingTable()).toBeTruthy();
+  });
+});
