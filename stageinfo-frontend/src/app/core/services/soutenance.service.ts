f7c6f75c1c1062e2932e8b22f4c66d648b283e9f
Antoine Brahimi <antoine.brahimi@etu.umontpellier.fr>
2021-03-19 15:36:24 +0100

    Injection du service soutenance dans le component soutenance

--- stageinfo-frontend/src/app/core/services/soutenance.service.ts
+++ stageinfo-frontend/src/app/core/services/soutenance.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoutenanceService {
 
-  constructor() { }
+  private urlBase: string = 'http://localhost:3000';
+
+  private role:string = 'invite';
+
+  constructor(private httpClient: HttpClient) { }
+
+  setRole(newRole: string){
+    if(['admin', 'invite', 'etudiant','secretaire','respParcours','repEntreprise','tuteur'].includes(newRole)){
+      this.role = newRole;
+    }
+  }
+
+  getAllSoutenances(): Observable<any> {
+    return this.httpClient.get(this.urlBase+'/api/soutenance');
+  }
+
+  getSoutenanceById(id:any): Observable<any> {
+    return this.httpClient.get(this.urlBase+'/api/soutenance/'+id);
+  }
+
+  deleteSoutenanceById(id:any): Observable<any>{
+    return this.httpClient.delete(this.urlBase+'/api/soutenance/'+id);
+  }
 }
