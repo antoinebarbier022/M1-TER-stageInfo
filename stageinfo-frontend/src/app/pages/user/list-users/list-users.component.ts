e83327305cd2e56aa045f3efac838a5dc7f9eec8
Brahimi Antoine <antoine.brahimi@etu.umontpellier.fr>
2021-04-02 10:04:13 +0200

    Modifications import, passage en relatif

--- stageinfo-frontend/src/app/pages/user/list-users/list-users.component.ts
+++ stageinfo-frontend/src/app/pages/user/list-users/list-users.component.ts
@@ -6,7 +6,7 @@ import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/core/services/user.service';
 
 import { CommonListingTable } from 'src/app/shared/classes/common-listing-table';
-import { UserModel } from 'src/app/core/models/UserModel';
+import { UserModel } from '../../../core/models/userModel';
 
 @Component({
   selector: 'app-list-users',
