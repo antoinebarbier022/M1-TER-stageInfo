4203d0f3775b91eb4a560120cf4a709f03bf74fe
Brahimi Antoine <antoine.brahimi@etu.umontpellier.fr>
2021-04-05 11:28:11 +0200

    Création d'un validateur isRepresentant

--- stageinfo-frontend/src/app/core/validators/validators.ts
+++ stageinfo-frontend/src/app/core/validators/validators.ts
@@ -14,3 +14,14 @@ export let isStudentValidator = (userArray: Array<any>) : ValidatorFn => {
         return control.value !== undefined && array.includes(control.value)? null : {'isStudent': false};
     }
 }
+
+/*
+* Vérifie que le nom saisi est bien celui d'un représentant d'entreprise
+*/
+export let isRepresentantValidator = (userArray: Array<any>) : ValidatorFn => { 
+    return (control: AbstractControl) : {[key: string]: boolean} | null => {
+        let array = userArray.filter(x => x.role === 'etudiant')
+                            .map(x => x.prenom + ' ' + x.nom);
+        return control.value !== undefined && array.includes(control.value)? null : {'isStudent': false};
+    }
+}
