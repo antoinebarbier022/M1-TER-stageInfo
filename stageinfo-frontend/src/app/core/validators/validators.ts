d44c82e35191a0cdada1de456f2398cdacffc068
Brahimi Antoine <antoine.brahimi@etu.umontpellier.fr>
2021-04-05 11:08:55 +0200

    Création d'un validateur isStudent

--- stageinfo-frontend/src/app/core/validators/validators.ts
+++ stageinfo-frontend/src/app/core/validators/validators.ts
@@ -2,9 +2,16 @@
 /** Own validators are created here **/
 /*************************************/
 
-import { AbstractControl } from "@angular/forms";
+import { AbstractControl, ValidatorFn } from "@angular/forms";
 
-let isStudent = (control: AbstractControl) : {[key: string]: boolean} | null => {
-
-    return null;
+/*
+* Vérifie que le nom saisi est bien celui d'un étudiant
+*/
+let isStudentValidator = (userArray: Array<any>) : ValidatorFn { 
+    return (control: AbstractControl) : {[key: string]: boolean} | null => {
+        let array = userArray.filter(x => x.role === 'etudiant')
+                            .map(x => x.prenom + ' ' + x.nom);
+        console.log(array);
+        return control.value !== undefined && array.includes(control.value)? {'isStudent': true} : null;
+    }
 }
