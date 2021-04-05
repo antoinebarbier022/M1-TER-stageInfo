ed31d9518495c794be47639ed5f68232b2ba807e
Brahimi Antoine <antoine.brahimi@etu.umontpellier.fr>
2021-04-05 10:53:23 +0200

    Ajout d'un dossier validators

---
+++ stageinfo-frontend/src/app/core/validators/validators.ts
@@ -0,0 +1,10 @@
+/*************************************/
+/** Own validators are created here **/
+/*************************************/
+
+import { AbstractControl } from "@angular/forms";
+
+let isStudent = (control: AbstractControl) : {[key: string]: boolean} | null => {
+
+    return null;
+}
