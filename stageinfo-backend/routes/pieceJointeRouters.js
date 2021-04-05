6fe82662e7600eb8f7759d2121c26e7eda2cdb7e
El-Mokhtar-AIssaoui <78304363+El-Mokhtar-AIssaoui@users.noreply.github.com>
2021-04-05 15:22:23 +0200

    Ajoute du multer sur pieceJointeRouters.js

--- stageinfo-backend/routes/pieceJointeRouters.js
+++ stageinfo-backend/routes/pieceJointeRouters.js
@@ -7,11 +7,12 @@ const Admin = require('../middleware/authAdmin');
 const router = express.Router();
 
 const pieceJointeCtrl = require('../controllers/pieceJointeController');
+const multer = require('../middleware/multer-config')
 
 router.get('/',Admin,pieceJointeCtrl.getAllPieceJointe);
 router.get('/:id',invite, pieceJointeCtrl.getOnePieceJointe);
 router.post('/',Etudiant, pieceJointeCtrl.createPieceJointe);
-router.put('/:id', Etudiant,pieceJointeCtrl.editPieceJointe);
+router.put('/:id', Etudiant,multer,pieceJointeCtrl.editPieceJointe);
 router.delete('/:id',Admin, pieceJointeCtrl.deleteOnePieceJointe);
 router.delete('/',Admin,pieceJointeCtrl.deleteAllPieceJointe);
 
