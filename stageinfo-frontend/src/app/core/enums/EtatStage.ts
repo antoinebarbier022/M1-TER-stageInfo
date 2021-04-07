741e398db0e6b58b6ac97eed2d9446dec7f2dce4
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-04-07 10:27:56 +0200

    on a défini une structure enum pour les roles et les états

--- stageinfo-frontend/src/app/core/enums/EtatStage.ts
+++ stageinfo-frontend/src/app/core/enums/EtatStage.ts
@@ -1,12 +1,14 @@
-export enum EtatStage {
-    propose = "Proposé",    // Le stage n'apparait pas dans le listing
-    valide = "Validé",      // Le stage est valide , donc il est libre 
-    refuse = "Refusé",       // Le stage est refusé
-    reserve = "Reservé",    // Le stage est reservé à un étudiant, il est maintenant en attente de l'affectation
-    //demande = "Demandé",    // une demande d'affectation d'un étudiant à un stage
-    affectEtudiant = "Étudiant affecté",     // Le stage est affecté à un étudiant
-    affectTuteur = "Tuteur affecté", // Les tuteurs peuvent choisir de tutoré le stage
-    affectRapporteur= "Rapporteur affecté", // le rapporteur est choisie par le tuteur du stage (le rapporteur est un prof qui n'est pas le tuteur lui même)
+// Convention : les type en majuscule -> https://google.github.io/styleguide/jsguide.html#features-objects-enums
 
-    termine = "Terminé" // Le stage à terminé une fois la soutenance passé (changement d'état en fonction de la date soutenance)
+export enum EtatStage {
+    PROPOSE = "propose",    // Le stage n'apparait pas dans le listing
+    VALIDE = "valide",      // Le stage est valide , donc il est libre 
+    REFUSE = "refuse",       // Le stage est refusé
+    RESERVE = "reserve",    // Le stage est reservé à un étudiant, il est maintenant en attente de l'affectation
+    //demande = "Demandé",    // une demande d'affectation d'un étudiant à un stage
+    AFFECT_ETUDIANT = "affectEtudiant",     // Le stage est affecté à un étudiant
+    AFFECT_TUTEUR = "affectTuteur", // Les tuteurs peuvent choisir de tutoré le stage
+    AFFECT_RAPPORTEUR= "affectRapporteur", // le rapporteur est choisie par le tuteur du stage (le rapporteur est un prof qui n'est pas le tuteur lui même)
+
+    TERMINE = "termine" // Le stage à terminé une fois la soutenance passé (changement d'état en fonction de la date soutenance)
   }
--- stageinfo-frontend/src/app/core/enums/RoleUser.ts
+++ stageinfo-frontend/src/app/core/enums/RoleUser.ts
@@ -1,5 +1,13 @@
+// Convention : les type en majuscule -> https://google.github.io/styleguide/jsguide.html#features-objects-enums
+
 export enum RoleUser {
-    Invite = 'Invite',
-    Etud = 'Etudiant',
-    Admin = 'Admin'
+    INVITE = 'invite',
+    ETUDIANT = 'etudiant',
+    TUTEUR = 'tuteur',
+    REPRESENTANT_ENTREPRISE ="repEntreprise",
+    RESPONSABLE_PARCOURS = 'respParcours',
+    RESPONSABLE_PEDAGOGIQUE = 'respPedagogique', // celui qui gère les stages
+    SECRETAIRE = 'secretaire',
+    ADMIN = 'admin'
+
   }
