6221452fa1841474bb4df5cad5e7e3e906748596
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-04-07 11:16:10 +0200

    J'avais oublié de modifier les roles dans les resolvers

--- stageinfo-frontend/src/app/core/resolves/all-etudiants.resolver.ts
+++ stageinfo-frontend/src/app/core/resolves/all-etudiants.resolver.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
+import { RoleUser } from '../enums/RoleUser';
 import { UserService } from '../services/user.service';
 
 @Injectable({
@@ -16,7 +17,7 @@ export class AllEtudiantsResolver implements Resolve<boolean> {
 
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>{
-    return this.userService.getAllUserByRole('etudiant').pipe(
+    return this.userService.getAllUserByRole(RoleUser.ETUDIANT).pipe(
       map(res => res),
       catchError((error) => {
         // redirection vers la page d'erreur 404 si le stage n'est pas trouvé
