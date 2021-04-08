2e6346d62e10c126ec97da50230e2fa5eb0d0e72
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-04-08 16:17:39 +0200

    suppressions de pages qui sont inutile (les pages edit/add ... de user, on utilise les modales depuis un petit moment)

--- stageinfo-frontend/src/app/pages/user/user.module.ts
+++ stageinfo-frontend/src/app/pages/user/user.module.ts
@@ -9,9 +9,6 @@ import { SharedModule } from '../../shared/shared.module';
 import { ProfileUserComponent } from './profile-user/profile-user.component';
 import { InfoUserComponent } from './info-user/info-user.component';
 import { ListUsersComponent } from './list-users/list-users.component';
-import { EditUserComponent } from './edit-user/edit-user.component';
-import { EditUserV2Component } from './edit-user-v2/edit-user-v2.component';
-import { AddUserComponent } from './add-user/add-user.component';
 import { ImportUsersComponent } from './import-users/import-users.component';
 import { ListEtudiantsComponent } from './list-etudiants/list-etudiants.component';
 
@@ -22,9 +19,6 @@ import { ListEtudiantsComponent } from './list-etudiants/list-etudiants.componen
     ProfileUserComponent,
     InfoUserComponent,
     ListUsersComponent,
-    EditUserComponent,
-    EditUserV2Component,
-    AddUserComponent,
     ImportUsersComponent,
     ListEtudiantsComponent],
     imports: [
@@ -38,9 +32,6 @@ import { ListEtudiantsComponent } from './list-etudiants/list-etudiants.componen
     ProfileUserComponent,
     InfoUserComponent,
     ListUsersComponent,
-    EditUserComponent,
-    EditUserV2Component,
-    AddUserComponent,
     ImportUsersComponent,
     ListEtudiantsComponent],
 })
