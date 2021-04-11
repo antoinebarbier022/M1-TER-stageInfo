8cb331618622ab07b5972804b510cb54e404095b
Brahimi Antoine <antoine.brahimi@etu.umontpellier.fr>
2021-04-11 10:28:31 +0200

    Ajout d'une route saisir-fiche-appreciation

--- stageinfo-frontend/src/app/app-routing.module.ts
+++ stageinfo-frontend/src/app/app-routing.module.ts
@@ -89,7 +89,7 @@ const routes: Routes = [
   { path: 'saisir-stage', component: AddStageComponent, canActivate: [AuthGuardService, RoleGuard]},
   { path: 'saisir-fiche-suivi', component: FicheSuiviComponent, canActivate: [AuthGuardService, RoleGuard], resolve: {allParcours: AllParcoursResolver, allUsers: AllUsersResolver}},
   { path: 'saisir-fiche-notation', component: FicheNotationComponent, canActivate: [AuthGuardService, RoleGuard], resolve: {allUsers: AllUsersResolver}},
-  { path: 'saisir-fiche-appreciation', component: FicheAppreciationStageComponent, canActivate: [AuthGuardService, RoleGuard], resolve: {allUsers: AllUsersResolver}},
+  { path: 'saisir-fiche-appreciation', component: FicheAppreciationComponent, canActivate: [AuthGuardService, RoleGuard], resolve: {allUsers: AllUsersResolver}},
 
   // route users
   { path: 'liste-utilisateurs', component: ListUsersComponent, canActivate: [AuthGuardService, RoleGuard], resolve: { users: AllUsersResolver, allParcours: AllParcoursResolver, allEntreprises: AllEntreprisesResolver }},
