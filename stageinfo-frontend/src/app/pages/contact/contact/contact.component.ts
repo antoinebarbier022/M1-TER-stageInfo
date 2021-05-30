30216ef365c9da09a55ab5c1fdd991a1a50af50d
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2021-05-30 17:23:57 +0200

    Ajout formulaire contact (il n'y a pas encore l'envoie d'email)

--- stageinfo-frontend/src/app/pages/contact/contact/contact.component.ts
+++ stageinfo-frontend/src/app/pages/contact/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  contactForm: FormGroup;
+  // @ts-ignore
+  errorMessage: String;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initContactComment();
+  
   }
+  initContactComment(){
+    this.contactForm = this.formBuilder.group({
+      message:['', Validators.required]
+    });
+  }
+
+  envoyerMessage(){
+    const formValue = this.contactForm.value;
+    var message :any = {
+      dateMessage: new Date(),
+      message: formValue['message'],
+    };
+    /**
+     * 
+     * TO DO !
+     */
+  }
+
 
 }
